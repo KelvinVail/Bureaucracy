33bb2f28a7471a8acce882946efec0aa79237803
Kelvin Vail <26182999+KelvinVail@users.noreply.github.com>
2022-04-11 18:13:29 +0100

    Outline usage

---
+++ src/Bureaucracy.Cli/Program.cs
@@ -0,0 +1,21 @@
+﻿var org = Organization.Create("MyOrg")
+    .With(Committee.Create("MyCommittee", frequency).MustApprove(Ticket.Type));
+
+var worker = Worker.Create("MyWorker", org);
+worker.CanComplete(Ticket.Type);
+worker.JoinCommittee("MyCommittee");
+
+var ticket = Ticket.Create<TicketType>();
+org.RaiseTicket(ticket);
+
+var project = Project.Create<ProjectType>("MyProject", noOfFeaures);
+org.ProposeProject(project);
+
+org.Simulate(logger);
+
+var ticketResult = org.TicketResult();
+var projectResult = org.ProjectResult();
+var timeCard = worker.TimeCard();
+
+
+
